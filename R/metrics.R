c0ec7c8c805ce0cb5014df6fe1d8336e4230e909
StefanoDamato <stefano.damato@idsia.ch>
2025-02-06 14:52:13 +0100

    Updated sQL (with faster LOO)

--- R/metrics.R
+++ R/metrics.R
@@ -1,18 +1,34 @@
-#quantile loss
+#' Quantile loss
+#'
+#' A proper scoring rule for probabilistic forecasts.
+#'
+#' @param actuals The realisation of the data.
+#' @param qforecast The predicted quantile.
+#' @param level The quantile level of the quantile.
+#' @return A vector containing tha value of the loss.
+#'
+#' @export
 quantile_loss <- function(actuals, qforecast, level){
   return(2*abs(actuals-qforecast)*ifelse(actuals>qforecast, level, 1-level));
 }
 
-#ql scaled in sample loo
+#' Quantile loss scaled by its in-sample LOO value.
+#'
+#' A proper and scale-independent scoring rule for probabilistic forecasts.
+#'
+#' @param actuals The realisation of the data.
+#' @param qforecast The predicted quantile.
+#' @param level The quantile level of the quantile.
+#' @param y_insample In-sample values of the time series.
+#' @return A vector containing tha value of the loss.
+#'
+#' @export
 quantile_loss_scaled <- function(actuals, qforecast, level, y_insample){
 
   y_insample <- sort(y_insample);
   K <- ceiling((length(y_insample)-1)*level);
-  scale = c(quantile_loss(y_insample[1:K], y_insample[K+1], level),
-            quantile_loss(y_insample[(K+1):length(y_insample)], y[K], level));
+  scale = c((y_insample[K+1]-y_insample[1:K])*level,
+            (y_insample[(K+1):length(y_insample)]-y[K])*(1-level));
 
   return(quantile_loss(actuals, qforecast, level)/mean(scale));
 }
-
-x = c(1,2,3,4)
-quantile(ecdf(x), 0.5001, type=1)
