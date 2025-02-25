10025e586e84faf358b17fc52ff632fb26aaedd7
StefanoDamato <stefano.damato@idsia.ch>
2025-02-25 08:47:56 +0100

    Included Brier score and log-loss

--- R/metrics.R
+++ R/metrics.R
@@ -5,7 +5,7 @@
 #' @param actuals The realisation of the data.
 #' @param qforecast The predicted quantile.
 #' @param level The quantile level of the quantile.
-#' @return A vector containing tha value of the loss.
+#' @return A vector containing the value of the loss.
 #'
 #' @family metrics
 #' @export
@@ -21,11 +21,11 @@ quantile_loss <- function(actuals, qforecast, level){
 #' @param qforecast The predicted quantile.
 #' @param level The quantile level of the quantile.
 #' @param y_insample In-sample values of the time series.
-#' @return A vector containing tha value of the loss.
+#' @return A vector containing the value of the loss.
 #'
 #' @family metrics
 #' @export
-quantile_loss_scaled <- function(actuals, qforecast, level, y_insample){
+scaled_quantile_loss <- function(actuals, qforecast, level, y_insample){
 
   y_insample <- sort(y_insample);
   K <- ceiling((length(y_insample)-1)*level);
@@ -33,4 +33,32 @@ quantile_loss_scaled <- function(actuals, qforecast, level, y_insample){
             (y_insample[(K+1):length(y_insample)]-y_insample[K])*(1-level));
 
   return(quantile_loss(actuals, qforecast, level)/(2*mean(scale)));
+}
+
+#' The Brier score.
+#'
+#' A quadratic loss on a binary classification problem.
+#'
+#' @param actuals The realisation of the data.
+#' @param prob_zero The predicted probability of zero.
+#' @return A vector containing the value of the loss.
+#'
+#' @family metrics
+#' @export
+brier <- function(actuals, prob_zero){
+  return((as.integer(actuals==0) - prob_zero)^2);
+}
+
+#' The log-loss.
+#'
+#' The negative log-likelihood of a Bernoulli random variable.
+#'
+#' @param actuals The realisation of the data.
+#' @param prob_zero The predicted probability of zero.
+#' @return A vector containing the value of the loss.
+#'
+#' @family metrics
+#' @export
+log_loss <- function(actuals, prob_zero){
+  return(log(ifelse(as.integer(actuals==0), prob_zero, 1-prob_zero)));
 }
