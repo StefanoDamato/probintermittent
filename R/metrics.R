3be568bad90f658cb7ce8d0af3820e5ad541acda
StefanoDamato <stefano.damato@idsia.ch>
2025-03-03 09:06:14 +0100

    Included 'log-score' function

--- R/metrics.R
+++ R/metrics.R
@@ -29,8 +29,8 @@ scaled_quantile_loss <- function(actuals, qforecast, level, y_insample){
 
   y_insample <- sort(y_insample);
   K <- ceiling((length(y_insample)-1)*level);
-  scale = c((y_insample[K+1]-y_insample[1:K])*level,
-            (y_insample[(K+1):length(y_insample)]-y_insample[K])*(1-level));
+  scale = c((y_insample[K+1]-y_insample[1:K])*(1-level),
+            (y_insample[(K+1):length(y_insample)]-y_insample[K])*level);
 
   return(quantile_loss(actuals, qforecast, level)/(2*mean(scale)));
 }
@@ -49,7 +49,7 @@ brier <- function(actuals, prob_zero){
   return((as.integer(actuals==0) - prob_zero)^2);
 }
 
-#' The log-loss.
+#' The binary log-loss.
 #'
 #' The negative log-likelihood of a Bernoulli random variable.
 #'
@@ -61,4 +61,23 @@ brier <- function(actuals, prob_zero){
 #' @export
 log_loss <- function(actuals, prob_zero){
   return(log(ifelse(as.integer(actuals==0), prob_zero, 1-prob_zero)));
+}
+
+
+#' The log-score.
+#'
+#' The log-likelihood of the forecast, evaluated on the data.
+#'
+#' @param actuals The realisation of the data.
+#' @param samples The predicted probability of zero.
+#' @return A vector containing the value of the loss.
+#'
+#' @family metrics
+#' @export
+log_score <- function(actuals, samples){
+  likelihood <- rep(NA, lenght(actuals));
+  for (i in 1:h){
+    likelihood[i] <-  mean(samples[i,] == actuals[i]);
+  }
+  return(log(likelihood));
 }
